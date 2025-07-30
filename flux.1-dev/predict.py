999b14ff8bec18f37be6ca9b925d439f8aa511d6
ParagEkbote <thecoolekbote189@gmail.com>
2025-07-30 08:18:32 +0000

    add the example for flux lora fast, and update pyproject.tom as well.

---
+++ flux.1-dev/predict.py
@@ -0,0 +1,80 @@
+import uuid
+from pathlib import Path
+
+from cog import BasePredictor, Input
+from diffusers import DiffusionPipeline, PipelineQuantizationConfig
+from PIL import Image
+from torchao.quantization import TorchAoConfig
+
+
+def save_image(image: Image.Image, output_dir: Path = Path("/tmp")) -> Path:
+    output_dir.mkdir(parents=True, exist_ok=True)
+    output_path = output_dir / f"{uuid.uuid4().hex}.png"
+    image.save(output_path)
+    return output_path
+
+class Predictor(BasePredictor):
+    def setup(self):
+        self.pipe = DiffusionPipeline.from_pretrained(
+            "black-forest-labs/FLUX.1-dev",
+            torch_dtype=torch.bfloat16,
+            quantization_config=PipelineQuantizationConfig(
+                quant_mapping={"transformer": TorchAoConfig("float8dq_e4m3_row")}
+            )
+        ).to("cuda")
+
+        self.pipe.enable_lora_hotswap(target_rank=8)
+
+        # Load default adapter
+        self.pipe.load_lora_weights(
+            "data-is-better-together/open-image-preferences-v1-flux-dev-lora",
+            weight_name="pytorch_lora_weights.safetensors",
+            adapter_name="open-image-preferences",
+            hotswap=True
+        )
+
+        self.second_lora_loaded = False
+
+        self.lora1_triggers = [
+            "Cinematic", "Photographic", "Anime", "Manga", "Digital art",
+            "Pixel art", "Fantasy art", "Neonpunk", "3D Model",
+            "Painting", "Animation", "Illustration"
+        ]
+        self.lora2_triggers = ["GHIBSKY"]
+
+    def predict(
+        self,
+        prompt: str = Input(description="The text prompt to generate the image from."),
+        trigger_word: str = Input(description="Style keyword that triggers a specific LoRA.")
+    ) -> Path:
+
+        # Adapter switching
+        if trigger_word in self.lora2_triggers and not self.second_lora_loaded:
+            self.pipe.load_lora_weights(
+                "aleksa-codes/flux-ghibsky-illustration",
+                weight_name="lora_v2.safetensors",
+                adapter_name="flux-ghibsky",
+                hotswap=True
+            )
+            self.second_lora_loaded = True
+
+        elif trigger_word in self.lora1_triggers and self.second_lora_loaded:
+            self.pipe.load_lora_weights(
+                "data-is-better-together/open-image-preferences-v1-flux-dev-lora",
+                weight_name="pytorch_lora_weights.safetensors",
+                adapter_name="open-image-preferences",
+                hotswap=True
+            )
+            self.second_lora_loaded = False
+
+        pipe_kwargs = {
+            "prompt": prompt,
+            "height": 1024,
+            "width": 1024,
+            "guidance_scale": 3.5,
+            "num_inference_steps": 28,
+            "max_sequence_length": 512,
+        }
+
+        image = self.pipe(**pipe_kwargs).images[0]
+        return save_image(image)
