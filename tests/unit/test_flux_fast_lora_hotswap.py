1f5bc5722b78d47d33d0e32a6c7201360c9a7150
Parag Ekbote <thecoolekbote189@gmail.com>
2025-10-03 15:17:13 +0000

    clean-up test strucuture

--- tests/unit/test_flux_fast_lora_hotswap.py
+++ tests/unit/test_flux_fast_lora_hotswap.py
@@ -1,15 +1,15 @@
 from io import BytesIO
 from pathlib import Path
 
+import pytest
 import requests
 import torch
 from PIL import Image
+from pytest import raises
 
-import pytest
 from models.flux_fast_lora_hotswap.predict import (
     Predictor,
     load_image,
     login_with_env_token,
     save_image,
 )
-from pytest import raises
